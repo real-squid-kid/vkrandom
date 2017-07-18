126c9592e5f5d195ec11e93b807e1a29ff324d1b
Vasily Makrov <fuzz4legal@mail.ru>
2017-07-18 18:02:18 +0300

    Add project files.

---
+++ VK_Randomizer/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.AddressTxt = New System.Windows.Forms.TextBox()
+        Me.LikesBtn = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Address:"
+        '
+        'AddressTxt
+        '
+        Me.AddressTxt.Location = New System.Drawing.Point(67, 10)
+        Me.AddressTxt.Name = "AddressTxt"
+        Me.AddressTxt.Size = New System.Drawing.Size(242, 20)
+        Me.AddressTxt.TabIndex = 1
+        '
+        'LikesBtn
+        '
+        Me.LikesBtn.Location = New System.Drawing.Point(16, 46)
+        Me.LikesBtn.Name = "LikesBtn"
+        Me.LikesBtn.Size = New System.Drawing.Size(104, 23)
+        Me.LikesBtn.TabIndex = 2
+        Me.LikesBtn.Text = "Get Likes Count"
+        Me.LikesBtn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(453, 109)
+        Me.Controls.Add(Me.LikesBtn)
+        Me.Controls.Add(Me.AddressTxt)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents AddressTxt As TextBox
+    Friend WithEvents LikesBtn As Button
+End Class
---
+++ VK_Randomizer/VkWorker.vb
@@ -0,0 +1,13 @@
+﻿Public Class VkWorker
+    Public Function GetLikes(e As String)
+        Dim temp As String
+        Dim parts(1) As String
+        ' Gets a string like https://vk.com/wall-29534144_6837921
+        ' Uses like this: https://api.vk.com/method/likes.getList?type=post&owner_id=-51016572&item_id=839965&filter=likes&offset=1
+        temp = Mid(e, 18)
+        parts = Split(temp, "_")
+        Dim webClient As New System.Net.WebClient
+        Dim result As String = webClient.DownloadString("https://api.vk.com/method/likes.getList?type=post&owner_id=" & parts(0) & "&item_id=" & parts(1) & "&filter=likes&offset=1")
+
+    End Function
+End Class
